2813b3ad5869435a53472fd99aaa506774357b52
Willem-Nieuwoudt <wnieuwoudt97@gmail.com>
2020-11-26 20:31:43 +0200

    Removed the sass package. Installed node-sass instead. Also installed styles-jsx-sass babel plugin. Created a custom babel preset to use this plugin. Changed NewsList component to use external styled-jsx with sass, instead of plain sass

--- client/src/components/NewsList.component.tsx
+++ client/src/components/NewsList.component.tsx
@@ -1,34 +1,36 @@
 import { INews } from '../interfaces/news.interface';
 import Link from 'next/link';
 
-import styles from '../styles/NewsList.module.scss';
-
-const NewsList = ({ news }: IProps) => {
-  return (
-    <div className={styles['news-list']}>
-      {news.map(({ id, url, title, points, comments_count }) => (
-        <div className={styles['news-item']} key={id}>
-          <h2 className={styles['news-item-title']}>
-            <a className={styles['news-item-title-a']} href={url}>
-              {title}
-            </a>
-          </h2>
-
-          <div className={styles['news-item-details']}>
-            <span className={styles['news-item-details-span']}>{points || 0} point(s)</span>
-
-            <Link href='/news/[id]' as={`/news/${id}`}>
-              <a className={styles['news-item-details-a']}>{comments_count || 0} comment(s)</a>
-            </Link>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+import { NewsListStyles } from '../styles/NewsList.styles';
 
 interface IProps {
   news: INews[];
 }
+
+const NewsList = ({ news }: IProps) => {
+  return (
+    <div className='news-list'>
+      {news.map(({ id, url, title, points, comments_count }) => (
+        <div className='news-item' key={id}>
+          <h2 className='news-item-title'>
+            <a className='news-item-title-a' href={url}>
+              {title}
+            </a>
+          </h2>
+
+          <div className='news-item-details'>
+            <span className='news-item-details-span'>{points || 0} point(s)</span>
+
+            <Link href='/news/[id]' as={`/news/${id}`}>
+              <a className='news-item-details-a'>{comments_count || 0} comment(s)</a>
+            </Link>
+          </div>
+        </div>
+      ))}
+
+      <style jsx>{NewsListStyles}</style>
+    </div>
+  );
+};
 
 export default NewsList;
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.scss';
+import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import nprogress from 'nprogress';
 import { useRouter } from 'next/router';
